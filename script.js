bcfac722a1f3e2d2a76f23f350ba091411638476
Oleksandra121 <o.stetsyshyna.346@studms.ug.edu.pl>
2024-06-10 13:03:08 +0300

    prob12

---
+++ script.js
@@ -0,0 +1,25 @@
+async function translateTerm() {
+    const term = document.getElementById('termInput').value.toLowerCase();
+    const languageSelect = document.getElementById('languageSelect').value;
+    const response = await fetch('dictionary.json');
+    const dictionary = await response.json();
+
+    let translation;
+    if (languageSelect === 'uk-pl') {
+        translation = dictionary[term];
+    } else if (languageSelect === 'pl-uk') {
+        translation = Object.keys(dictionary).find(key => dictionary[key] === term);
+    }
+
+    const translationElement = document.getElementById('translation');
+    if (translation) {
+        translationElement.innerHTML = languageSelect === 'uk-pl'
+            ? `${translation}`
+            : `${translation}`;
+    } else {
+        translationElement.textContent = 'Переклад не знайдено';
+    }
+}
+
+/*? `Польський переклад: <span class="highlight">${translation}</span>`
+            : `Український переклад: <span class="highlight">${translation}</span>`;*/
